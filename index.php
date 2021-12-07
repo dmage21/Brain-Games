edb57aa96e301a9849985292a5b293fa6278ae04
dmage21 <95660793+dmage21@users.noreply.github.com>
2021-12-06 22:08:04 -0500

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
  <h1> <a href="index.php">Brain Games </a>  <img src="brainart.png" width="200" height="175"> </h1>
 </left>
 <center>
-<h2> <a href="phase3.php">Home</a>            &nbsp; &nbsp; &nbsp; 
+<h2> <a href="index.php">Home</a>            &nbsp; &nbsp; &nbsp; 
 <a href="signup.php">Sign Up</a>             &nbsp; &nbsp; &nbsp;
 <a href="categories.php">About Us</a>  &nbsp; &nbsp; &nbsp;
 <a href="search.php">Donate Now</a>              &nbsp; &nbsp; &nbsp; 
@@ -22,9 +22,13 @@ if (isset($_SESSION["loggedin"])) {
 echo "<a href='logout.php'>Logout</a> ";
 }else {echo "<a href='login.html'>Login</a> &nbsp; &nbsp; &nbsp	";}
 ?>
- <center> <br> <br> <br> <br> </center>
- <left> <h2> Game 1</h2> </left>
- <left> <a href="gamescreen.php"> <img src="game1.png" width="200" height="200"> </a> </left>
+</center>
+<center> <br> <br> <br> <br> </center>
+<left> <h2> Game 1</h2> </left>
+<left> <a href="gamescreen.php"> <img src="game1.png" width="200" height="200"> </a> </left>
+<left> <a href="gamescreen2.php"> <img src="game2.png" width="200" height="200"> </a> </left>
+<br>
+
 <center> <h3> <div> Welcome to Garfield's Online Bookstore! Garfield's Books was established in 1998,<br> and has since
 grown a large collection of books. This webiste allows you to<br>
  browse a vast online bookstore as well as make purchases that will be delivered to you! </div> </h3> </center>
