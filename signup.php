d742a5ad8aa0d90a1f726b1a3139799600b67a1d
dmage21 <95660793+dmage21@users.noreply.github.com>
2021-12-06 23:24:33 -0500

    Update signup.php

--- signup.php
+++ signup.php
@@ -32,10 +32,7 @@ echo "<a href='logout.php'>Logout</a> ";
 			<form name="Sign Up Form" action="signupinsert.php" method="post">
 <h2> Sign Up! </h2> <br>
 <h3>
-First Name: <input type="text" name ="first_name" id="1" size="25"
-maxlength="25" value=""> <br> <br>
-
-Last Name: <input type="text" name ="last_name" id="2" size="25"
+Username: <input type="text" name ="user" id="1" size="25"
 maxlength="25" value=""> <br> <br>
 
 Email Address: <input type="text" name ="email" id="3" size="25"
@@ -44,17 +41,7 @@ maxlength="50" value=""> <br> <br>
 Password: <input type="password" name ="password" id="4" size="25"
 maxlength="" value=""> <br> <br>
 
-Address: <input type="text" name ="address" id="5" size="25"
-maxlength="25" value=""> <br> <br>
 
-City:<input type="text" name ="city" id="6" size="25"
-maxlength="25" value=""> <br> <br>
-
-State:<input type="text" name ="state" id="7" size="25"
-maxlength="25" value=""> <br> <br>
-
-Zip Code: <input type="number" name ="zipcode" id="1" size="25"
-maxlength="25" value=""> <br> <br>
 			<input type="submit" name="submit" value="Submit" />
 	<input type="reset" name="reset" value="Cancel" />
 <script>
