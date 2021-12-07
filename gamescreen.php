625fb30c61521b94031aeac0cad7dbf52e901309
kdodr998 <95663149+kdodr998@users.noreply.github.com>
2021-12-07 01:13:40 -0500

    Update gamescreen.php

--- gamescreen.php
+++ gamescreen.php
@@ -64,6 +64,7 @@ var myGameArea = {
         this.canvas.width = 480;
         this.canvas.height = 270;
         this.context = this.canvas.getContext("2d");
+		document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.frameNo = 100;
         this.interval = setInterval(updateGameArea, 20);
         },
