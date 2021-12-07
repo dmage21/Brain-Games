409ee12ac1bfa3b85bba42b3ffd8d584cb0185bb
dmage21 <95660793+dmage21@users.noreply.github.com>
2021-12-06 21:14:46 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html> 
+
+<head>
+<!-- Referencing to the style sheet --> 
+<link rel ="stylesheet" href="stylesheet.css">
+</head>
+<body>
+<!-- Making the links at the top of the page. -->
+<center>
+<h4> <a href="phase3.php"> Home</a>            &nbsp; &nbsp; &nbsp; 
+<a href="signup.php"> Sign up </a>             &nbsp; &nbsp; &nbsp;
+<a href="categories.php"> Browse Categories </a>  &nbsp; &nbsp; &nbsp;
+<a href="search.php"> Search </a>              &nbsp; &nbsp; &nbsp; 
+<a href="shoppingcart.php"> Shopping Cart </a> &nbsp; &nbsp; &nbsp; 
+<a href="orders.php"> View Orders </a>         &nbsp; &nbsp; &nbsp;
+<a href="contact.php"> Contact Us </a>         &nbsp; &nbsp; &nbsp;
+<?php
+session_start();
+if (isset($_SESSION["loggedin"])) {
+echo "<a href='logout.php'> Logout </a> ";
+}else {echo "<a href='login.html'> Login </a> &nbsp; &nbsp; &nbsp	";}
+?>
+</center> <br> <br>
+<center> <img src="http://unixweb.kutztown.edu/~kdodr959/Project/Phase1/images/garfieldbook.jpg" width = "250" height="300"> </center>
+<center> <h2> <div> Welcome to Garfield's Online Bookstore! Garfield's Books was established in 1998,<br> and has since
+grown a large collection of books. This webiste allows you to<br>
+ browse a vast online bookstore as well as make purchases that will be delivered to you! </div> </h2> </center>
+<br> <br>
+<center> <h3> This is a school project, not an actual store. </h3> </center> 
+
+</body>
+</html>
