dd054f7f3063adfa613482cddfed61cd20fb7e63
dmage21 <95660793+dmage21@users.noreply.github.com>
2021-12-06 21:57:57 -0500

    Update index.php

--- index.php
+++ index.php
@@ -5,25 +5,25 @@
 <!-- Referencing to the style sheet --> 
 <link rel ="stylesheet" href="stylesheet.css">
 </head>
-<body>
+<body style="background-color:grey;">
 <!-- Making the links at the top of the page. -->
 <left>
  <h1> <a href="index.php">Brain Games </a>  <img src="brainart.png" width="200" height="175"> </h1>
 </left>
 <center>
-<h2> <a href="phase3.php"> Home</a>            &nbsp; &nbsp; &nbsp; 
-<a href="signup.php"> Sign Up </a>             &nbsp; &nbsp; &nbsp;
-<a href="categories.php"> About Us </a>  &nbsp; &nbsp; &nbsp;
-<a href="search.php"> Donate Now </a>              &nbsp; &nbsp; &nbsp; 
+<h2> <a href="phase3.php">Home</a>            &nbsp; &nbsp; &nbsp; 
+<a href="signup.php">Sign Up</a>             &nbsp; &nbsp; &nbsp;
+<a href="categories.php">About Us</a>  &nbsp; &nbsp; &nbsp;
+<a href="search.php">Donate Now</a>              &nbsp; &nbsp; &nbsp; 
 
 <?php
 session_start();
 if (isset($_SESSION["loggedin"])) {
-echo "<a href='logout.php'> Logout </a> ";
-}else {echo "<a href='login.html'> Login </a> &nbsp; &nbsp; &nbsp	";}
+echo "<a href='logout.php'>Logout</a> ";
+}else {echo "<a href='login.html'>Login</a> &nbsp; &nbsp; &nbsp	";}
 ?>
 <center> <br> <br>
-<center> <img src="http://unixweb.kutztown.edu/~kdodr959/Project/Phase1/images/garfieldbook.jpg" width="200" height="250"> </center>
+ <left> <a href="gamescreen.php"> <img src="game1.png" width="200" height="200"> </a> </left>
 <center> <h3> <div> Welcome to Garfield's Online Bookstore! Garfield's Books was established in 1998,<br> and has since
 grown a large collection of books. This webiste allows you to<br>
  browse a vast online bookstore as well as make purchases that will be delivered to you! </div> </h3> </center>
