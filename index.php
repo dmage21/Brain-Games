621964e214fad4a361e554c0d202ee914e0b5205
kdodr998 <95663149+kdodr998@users.noreply.github.com>
2022-04-06 23:46:25 -0400

    Update index.php

--- index.php
+++ index.php
@@ -7,23 +7,24 @@
 </head>
 
 <!-- Making the links at the top of the page. -->
-<left>
-<h1> <a href="index.php">Brain Games </a>  <img src="brainart.png" width="200" height="175"> </h1>
-</left>
 <center>
 <div class="topnav">
+  <a href="index.php">Brain Games </a> 
   <a class="active" href="index.php>Home</a>
-  <a href="donate.php">Donate</a>
   <a href="#contact">Contact</a>
   <a href="singup.php">Sign up</a>
-  <a href="aboutus.php">About</a>
 	<?php
 	session_start();
 	if (isset($_SESSION["loggedin"])) {
 	echo "<a href='logout.php'>Logout</a> ";
 	}else {echo "<a href='login.php'>Login</a> &nbsp; &nbsp; &nbsp	";}
 	?>
+  <a href="aboutus.php">About</a>
+  <a href="donate.php">Donate</a>
 </div>
+<left>
+<img src="brainart.png" width="200" height="175"> </h1>
+</left>
 
 </center>
 <center> <br> <br> <br> <br> </center>
