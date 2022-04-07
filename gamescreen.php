32ef04ada1955795834cf4384915924affc97869
kdodr998 <95663149+kdodr998@users.noreply.github.com>
2022-04-07 00:02:59 -0400

    4244

--- gamescreen.php
+++ gamescreen.php
@@ -24,66 +24,6 @@
 <left>
 <img src="brainart.png" width="200" height="175"> </h1>
 </left>
-<style>
-canvas {
-	border:5px solid #d3d3d3;
-	background-color: #f1f1f1;
-	padding: 0;
-	margin: auto;
-	display: block;
-	position: absolute;
-	top: 0;
-	bottom: 0;
-	left: 0;
-	right: 0;
-}
-
-label {
-	user-select: none;
-}
-
-#debugPane {
-	background: #0002;
-	border: 1px solid black;
-	display: flex;
-	flex-wrap: wrap;
-	margin: 1rem 1rem 0 1rem;
-	padding: 0.5rem;
-} #debugPane.hidden {
-	display: none;
-}
-
-#debugPane > label, #debugPane > div {
-	display: block;
-	flex: 1 0 auto;
-	padding: 0 0.25rem;
-	width: 50%;
-	box-sizing: border-box;
-}
-
-#debugPane > label {
-	text-align: right;
-}
-
-#debugPane > div {
-	text-align: left;
-}
-
-#gameZone {
-	display: flex;
-	flex-direction: column;
-	padding: 0 calc(50% - 16rem);
-}
-
-#title {
-	margin-top: 1rem;
-}
-
-#userGuess {
-	margin: 1rem 0 0.5rem 0;
-	text-align: center;
-}
-</style>
 
 		</center>
 		<center id="gameZone">
