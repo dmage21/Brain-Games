2650063ecf1dde35c87f31d336b3c7a70ce8913d
kdodr998 <95663149+kdodr998@users.noreply.github.com>
2022-04-07 22:29:55 -0400

    Update index.php

--- index.php
+++ index.php
@@ -10,8 +10,8 @@
 <center>
 <div class="topnav">
   <a href="index.php">Brain Games </a> 
-  <a class="active" href="index.php>Home</a>
-  <a class="active" href="contact.php">Contact</a>
+  <a href="index.php>Home</a>
+  <a href="contact.php">Contact</a>
   <a href="signup.php" style="float:right;" >Sign up</a>
 	<?php
 	session_start();
