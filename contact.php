08f316e61d3bdd1353a2b2c43141eaf9b5a018bc
kdodr998 <95663149+kdodr998@users.noreply.github.com>
2022-04-07 21:53:05 -0400

    contact us page

---
+++ contact.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html> 
+<head>
+<!-- Referencing to the style sheet --> 
+<link rel ="stylesheet" href="stylesheet.css">
+</head>
+
+<!-- Making the links at the top of the page. -->
+<center>
+<div class="topnav">
+  <a href="index.php">Brain Games </a> 
+  <a class="active" href="index.php>Home</a>
+  <a href="#contact">Contact</a>
+  <a href="signup.php" style="float:right;" >Sign up</a>
+	<?php
+	session_start();
+	if (isset($_SESSION["loggedin"])) {
+	echo "<a href='logout.php' >Logout</a> ";
+	}else {echo "<a href='login.php' >Login</a> &nbsp; &nbsp; &nbsp	";}
+	?>
+  <a href="aboutus.php">About</a>
+  <a href="donate.php">Donate</a> 
+</div>
+<left>
+<img src="brainart.png" width="200" height="175"> </h1>
+</left>
+
+<h1> Contact Us! <h1> 
+<h2> Our Email Address: Garfieldbookstore@gmail.com <br> <br>
+Our Phone Number: 555-543-2198 <br> <br>
+</h2>
+	<form name="Contact Form" action="" method="">
+<h2> Fill out your question or concern in the box below, and it will be emailed directly to us! <h2>
+          
+          <textarea name = "contact" id = "8" cols="80" rows="10" >
+		  </textarea> <br>
+			<button onclick="popUp()">Submit</button>
+			<button onclick="popUp()">Clear</button>
+<script>
+function popUp() {
+	confirm("Are you sure?");
+}
+</script>	  
+        </form
+</body>
+</html>
