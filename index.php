0822ca5c8b52674c2673c662d90310138915ab1b
kdodr998 <95663149+kdodr998@users.noreply.github.com>
2022-04-07 22:31:01 -0400

    Update index.php

--- index.php
+++ index.php
@@ -11,8 +11,8 @@
 <div class="topnav">
   <a href="index.php">Brain Games </a> 
   <a href="index.php>Home</a>
-  <a href="contact.php">Contact</a>
   <a href="signup.php" style="float:right;" >Sign up</a>
+  <a href="contact.php">Contact</a>
 	<?php
 	session_start();
 	if (isset($_SESSION["loggedin"])) {
