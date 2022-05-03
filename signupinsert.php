e5c77982692ef861872d5b122c4b6913c5b2bcb7
kdodr998 <95663149+kdodr998@users.noreply.github.com>
2022-05-03 18:36:55 -0400

    Update signupinsert.php

--- signupinsert.php
+++ signupinsert.php
@@ -33,7 +33,7 @@ $password = $_POST['password'];
 // variables to connect to server
 $DB_HOST = "ec2-3-218-171-44.compute-1.amazonaws.com";
 $DB_USER = "zbesxlnsnqhoam";
-$DB_PORT = "5432"
+$DB_PORT = "5432";
 $DB_PASS = "641fb695885f0e0e22ee82ce9d624848a33d2c7ba1e76d517c5f2d288f35f77f";
 $DB_NAME = "dd5ivembuns2tk";
 
