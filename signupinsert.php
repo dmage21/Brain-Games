0d02095b7a1eba4c58b2cb05d8fced2f9e152273
kdodr998 <95663149+kdodr998@users.noreply.github.com>
2022-05-03 18:23:14 -0400

    Update signupinsert.php

--- signupinsert.php
+++ signupinsert.php
@@ -37,10 +37,9 @@ $DB_PASS = "641fb695885f0e0e22ee82ce9d624848a33d2c7ba1e76d517c5f2d288f35f77f";
 $DB_NAME = "dd5ivembuns2tk";
 
 //conncecting to server
-echo $user ;
 
 $conn = pg_connect($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME) or die ("could not connect to server");
-
+echo $user ;
 if (!$conn){
 	echo "Connection failed:";
 }
