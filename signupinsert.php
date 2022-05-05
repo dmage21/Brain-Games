e8a58ada899dcb678bee0f58fc5524890885259f
kdodr998 <95663149+kdodr998@users.noreply.github.com>
2022-05-04 21:52:08 -0400

    Update signupinsert.php

--- signupinsert.php
+++ signupinsert.php
@@ -41,8 +41,8 @@ $DB_NAME = "dd5ivembuns2tk";
 //conncecting to server
 
 echo "<center> <h3> Signed up Successfully!</h3>";
-	<h3> echo "Hello";
-	echo $user<h3>;
+	echo "<h3> Hello </h3>";
+	echo "<h3>" . $user ."</h3>";
 	echo "<br> <br> <h3> Log in to your new account: <a href='login.html'> Login </a>";
 
 //$conn = pg_connect("host=$DB_HOST, dbname=$DB_NAME, user=$DB_USER, password=$DB_PASS") 
