6c93ee070c4ac98a922823b3b73e8a8fb00786df
kdodr998 <95663149+kdodr998@users.noreply.github.com>
2022-05-04 21:54:36 -0400

    !

--- gamescreen2.php
+++ gamescreen2.php
@@ -8,22 +8,28 @@
 
 		<!-- Making the links at the top of the page. -->
 		<center>
-		<div class="topnav">
-			<a href="index.php">Brain Games </a> 
-			<a href='signup.php' style="float:right;" >Sign up</a>
-			<a href="contact.php">Contact</a>
-			<?php
-			session_start();
-			if (isset($_SESSION["loggedin"])) {
-			echo "<a href='logout.php' >Logout</a> ";
-			}else {echo "<a href='login.php' >Login</a> &nbsp; &nbsp; &nbsp	";}
-			?>
-			<a href="aboutus.php">About</a>
-			<a href="donate.php">Donate</a> 
-		</div>
-			<left>
-				<img src="brainart.png" width="200" height="175"> </h1> 
-			</left>
+<div class="topnav">
+  <a href="index.php">Brain Games </a> 
+  <a href='signup.php' >Sign up</a>
+  <a href="contact.php">Contact</a>
+  <a href="aboutus.php">About</a>
+  <a href="donate.php">Donate</a> 
+	<?php
+	session_start();
+	if (isset($_SESSION["loggedin"])) {
+	echo "<a href='logout.php' style=float:right>Logout</a> ";
+	}else {echo "<a href='login.php' >Login</a> &nbsp; &nbsp; &nbsp	";}
+	?>
+</div>
+<left>
+<img src="brainart.png" width="200" height="175"> </h1> 
+<?php
+session_start();
+if (isset($_SESSION["loggedin"])) 
+{
+	echo "<center> <h2> Hello, ". $_SESSION["user"] . " </h2> </center>";
+}
+	?>
 		</center>
 		<center id="gameZone">
 			<label>Current score:</label>
