03ebec86d9ddff34df6b8bb6983d402dc4a367c4
nrew088 <98899180+nrew088@users.noreply.github.com>
2022-05-04 20:39:34 -0400

    Add files via upload

--- gamescreen2.php
+++ gamescreen2.php
@@ -1,34 +1,34 @@
 <!DOCTYPE html>
 <html> 
-<head>
-<!-- Referencing to the style sheet --> 
-<link rel ="stylesheet" href="cryptogame.css">
-<script src="cryptogame2.js" async defer></script>
-</head>
+	<head>
+		<!-- Referencing to the style sheet --> 
+			<link rel ="stylesheet" href="cryptogame.css">
+		<script src="cryptogame2.js" async defer></script>
+	</head>
 
-<!-- Making the links at the top of the page. -->
-<center>
-<div class="topnav">
-  <a href="index.php">Brain Games </a> 
-  <a href='signup.php' style="float:right;" >Sign up</a>
-  <a href="contact.php">Contact</a>
-	<?php
-	session_start();
-	if (isset($_SESSION["loggedin"])) {
-	echo "<a href='logout.php' >Logout</a> ";
-	}else {echo "<a href='login.php' >Login</a> &nbsp; &nbsp; &nbsp	";}
-	?>
-  <a href="aboutus.php">About</a>
-  <a href="donate.php">Donate</a> 
-</div>
-<left>
-<img src="brainart.png" width="200" height="175"> </h1> 
-</left>
-
-
+		<!-- Making the links at the top of the page. -->
+		<center>
+		<div class="topnav">
+			<a href="index.php">Brain Games </a> 
+			<a href='signup.php' style="float:right;" >Sign up</a>
+			<a href="contact.php">Contact</a>
+			<?php
+			session_start();
+			if (isset($_SESSION["loggedin"])) {
+			echo "<a href='logout.php' >Logout</a> ";
+			}else {echo "<a href='login.php' >Login</a> &nbsp; &nbsp; &nbsp	";}
+			?>
+			<a href="aboutus.php">About</a>
+			<a href="donate.php">Donate</a> 
+		</div>
+			<left>
+				<img src="brainart.png" width="200" height="175"> </h1> 
+			</left>
 		</center>
 		<center id="gameZone">
-			<label for="debugToggle">Debug</label>
+			<label>Current score:</label>
+			<div id="scoreCount">0</div>
+			<label for="debugToggle">Check your answer</label>
 			<input type="checkbox" id="debugToggle" checked="true"></input>
 			<div id="debugPane" class="hidden">
 				<label>Correct answer:</label>
