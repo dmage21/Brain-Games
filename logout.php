2b4431f603681c81b61e7eaf08d176fbb35b5eba
kdodr998 <95663149+kdodr998@users.noreply.github.com>
2022-05-04 21:16:02 -0400

    Create logout.php

---
+++ logout.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html> 
+
+<head>
+<!-- Referencing to the style sheet --> 
+<link rel ="stylesheet" href="stylesheet.css">
+</head>
+
+<!-- Making the links at the top of the page. -->
+<center>
+<div class="topnav">
+  <a href="index.php">Brain Games </a> 
+  <a href='signup.php' >Sign up</a>
+  <a href="contact.php">Contact</a>
+  <a href="aboutus.php">About</a>
+  <a href="donate.php">Donate</a> 
+	<?php
+	session_start();
+	if (isset($_SESSION["loggedin"])) {
+	echo "<a href='logout.php' style=float:right>Logout</a> ";
+	}else {echo "<a href='login.php' >Login</a> &nbsp; &nbsp; &nbsp	";}
+	?>
+</div>
+<left>
+<img src="brainart.png" width="200" height="175"> </h1> 
+
+<?php
+if(session_status()==2){
+session_destroy();
+echo"<br><br> <br> <br> <h2> You have been logged out successfully </h2>";
+}
+?>
